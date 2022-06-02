a51eb4a5927da4a8066f4858d4e4912ed20e141d
Reda <74600085+reda98494@users.noreply.github.com>
2022-06-02 14:19:04 +0200

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "The page was created by the user data" | sudo tee /var/www/html/index.html
+yum update -y
+yum install -y httpd
+systemctl enable --now httpd git
+
+cd /var/www/
+git clone <##GIT_URL##>
