40c99ffec67c3bbcb0248d18ef0dc4ac5f325d10
Reda <74600085+reda98494@users.noreply.github.com>
2022-06-02 13:21:58 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-west-3"
-  access_key = <##ACCESS_KEY##>
-  secret_key = <##SECRET_KEY##>
+  access_key = "AKIA6AIDAZZFZSPVY3AM"
+  secret_key = "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" 
  
 }
