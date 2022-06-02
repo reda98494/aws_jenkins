663929ef1097e635e8827cf7544c4533486e9a5d
Reda <74600085+reda98494@users.noreply.github.com>
2022-06-02 12:37:08 +0200

    Update admin_instance.tf

--- admin_instance.tf
+++ admin_instance.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "admin_machine_reda" {
+resource "aws_instance" "JENKINS-instance-adm" {
         key_name = "test_keypair"
         ami = "ami-021d41cbdefc0c994"
         security_groups = ["sg-092271493b7c6d49a"]
         subnet_id = "subnet-0c59a465aaea21d4a"
         instance_type = "t2.micro"
         associate_public_ip_address = "true"
-        user_data = "<##USER_DATA#>"
+        user_data = "${file("<##USER_DATA##>")}"
         tags = {
                 Name = "admin_machine_reda"
         }
